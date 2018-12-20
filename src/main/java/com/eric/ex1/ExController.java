e7a0ad396539c8fb387572c688d1ef84f47ed452
EricKrg <eric.krueger@uni-jena.de>
2018-12-20 14:38:03 +0100

    added yaml and properties config

--- src/main/java/com/eric/ex1/ExController.java
+++ src/main/java/com/eric/ex1/ExController.java
@@ -1,6 +1,7 @@
 package com.eric.ex1;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import services.NotificationServices;
@@ -23,5 +24,25 @@ public class ExController {
 
     @RequestMapping("/notify")
     public String notifyServ() { return this.notificationServices.toString(); }
+
+    //app.properties
+    @Value("${controller.msg}")
+    private String controllerMsg;
+    @RequestMapping("/prop")
+    public String appProps(){ return  controllerMsg; }
+    // from yaml
+    @Value("${messages.greet}")
+    private String greet;
+    @Value("${messages.bye}")
+    private  String bye;
+    @RequestMapping("/yaml")
+    public String appYamTalk(){ return greet + " and " + bye; }
+
+    //random values from props.
+    @Value("${my.secret}")
+    private  String secret;
+    @RequestMapping("/secret")
+    public String randSecret(){ return this.secret; }
+
 
 }
